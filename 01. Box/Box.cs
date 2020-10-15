445aa6a850453342362ade0bf6bab6b173d5eed5
Ahmed Softev <ahmed_v@abv.bg>
2020-10-15 16:04:38 +0300

    C#

---
+++ 01. Box/Box.cs
@@ -0,0 +1,35 @@
+﻿using NPOI.SS.Formula.Functions;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+
+        private Stack<T> stack;
+
+        public Box()
+        {
+            stack = new Stack<T>();
+        }
+
+        public int Count
+        {
+            get
+            {
+                return stack.Count;
+            }
+        }
+
+        public void Add(T element) => stack.Push(element);
+        
+
+        public T Remove() => stack.Pop();
+        
+
+    }
+
+    
+}
